c8764c46931bef107311a21eddc8ebb11e801940
dmeteo <140282390+dmeteo@users.noreply.github.com>
2026-05-08 03:02:56 +0500

    refactor: products schemas

--- backend/app/schemas/products.py
+++ backend/app/schemas/products.py
@@ -8,7 +8,7 @@ class Review(BaseModel):
     user_name: str
     product_id: int
     rate: int = Field(ge=1, le=5)
-    text: str | None
+    text: str | None = None
 
     
 class ProductPage(BaseModel):
@@ -17,11 +17,16 @@ class ProductPage(BaseModel):
     title: str
     images: list[str]
     description: str
-    price: Decimal
-    discount_percent: Decimal
-    final_price: Decimal
-    rating: Decimal | None
-    reviews_count: int
+    price: Decimal = Field(gt=0, examples=["199.99"])
+    discount_percent: Decimal = Field(ge=0, le=100, examples=["10.5"])
+    final_price: Decimal = Field(gt=0, examples=["199.99"])
+    rating: float | None = Field(
+        default=None,
+        ge=0,
+        le=5,
+        examples=[4.8],
+    )
+    reviews_count: int 
     quantity: int
     categories: list[str]
     reviews: list[Review]
@@ -31,12 +36,17 @@ class ProductCard(BaseModel):
     id: int
     title: str
     images: list[str]
-    price: Decimal
-    discount_percent: Decimal
-    final_price: Decimal
-    rating: Decimal | None
-    reviews_count: int
-    quantity: int
+    price: Decimal = Field(gt=0, examples=["199.99"])
+    discount_percent: Decimal = Field(ge=0, le=100, examples=["10.5"])
+    final_price: Decimal = Field(gt=0, examples=["199.99"])
+    rating: float | None = Field(
+        default=None,
+        ge=0,
+        le=5,
+        examples=[4.8],
+    )
+    reviews_count: int = Field(ge=0)
+    quantity: int = Field(ge=0)
     
 
 class ProductCreateRequest(BaseModel):
@@ -44,9 +54,9 @@ class ProductCreateRequest(BaseModel):
     title: str = Field(max_length=30)
     description: str = Field(max_length=300)
     images: list[str]
-    price: Decimal = Field(gt=0)
+    price: Decimal = Field(gt=0, max_digits=10, decimal_places=2, examples=["100.99"])
     categories: list[str]
-    discount_percent: Decimal = Field(default=0, ge=0, le=100)
+    discount_percent: Decimal = Field(default=0, ge=0, le=100, max_digits=5, decimal_places=2, examples=["10.5"])
     quantity: int = Field(ge=0)
     is_active: bool
 
@@ -66,4 +76,3 @@ class ProductsResponse(BaseModel):
 
 class ReviewsResponse(BaseModel):
     reviews: list[Review]
-    
