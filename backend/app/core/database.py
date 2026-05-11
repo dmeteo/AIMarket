fbb4622a6c160bd0850b889c235511a3cd31681a
dmeteo <140282390+dmeteo@users.noreply.github.com>
2026-05-11 09:23:24 +0500

    feat: add BaseModelMixin and user models

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,15 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
 
 engine = create_engine(settings.DB_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine)
-
-
-class Base(DeclarativeBase):
-    pass
 
 
 def get_db():
