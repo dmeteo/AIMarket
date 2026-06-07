b935cb460b9976de4917ca6be9601bce22037b46
dmeteo <pesh.dmetro@gmail.com>
2026-06-07 08:10:15 +0500

    feat: add Cart models, make migration

---
+++ backend/app/models/cart.py
@@ -0,0 +1,29 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship, validates
+
+from app.models.base import Base, BaseModelMixin
+
+
+class Cart(BaseModelMixin):
+    __tablename__ = "carts"
+    
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    
+    items: Mapped[list["CartItem"]] = relationship(back_populates="cart")
+    
+
+class CartItem(Base):
+    __tablename__ = "cart_items"
+    
+    cart_id: Mapped[int] = mapped_column(ForeignKey("carts.id"), primary_key=True)
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), primary_key=True)
+    quantity: Mapped[int] = mapped_column(default=1, nullable=False)
+    
+    cart: Mapped["Cart"] = relationship(back_populates="items")
+    
+    @validates("quantity")
+    def validate_quantity(self, key, quantity):
+        if quantity < 1:
+            raise ValueError("Quantity was ge 1")
+        
+        return quantity
