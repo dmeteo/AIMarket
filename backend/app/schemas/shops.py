b3ff0e705fb4427cdf0b134803db43909433ecf9
dmeteo <pesh.dmetro@gmail.com>
2026-06-13 17:03:16 +0500

    fix: fix build_url with None, added None to schema

--- backend/app/schemas/shops.py
+++ backend/app/schemas/shops.py
@@ -5,7 +5,7 @@ from app.schemas.products import ProductCard
 
 class ShopResponse(BaseModel):
     id: int
-    logo_url: str
+    logo_url: str | None = None
     title: str
     description: str | None = None
     favourites_count: int = Field(ge=0)
@@ -21,7 +21,7 @@ class ShopForUserResponse(BaseModel):
 
 class ShopCreateRequest(BaseModel):
     title: str = Field(min_length=3, max_length=30)
-    logo_url: str
+    logo_url: str | None = None
     description: str | None = Field(default = None, max_length=300)
     
     
